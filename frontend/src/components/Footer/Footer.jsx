be07ac6eb8ae1af2878c1c5b314ce30f6da5cc5a
HARSH GARG <officialharshgarg>
2024-08-10 17:05:50 +0530

    Created Cart component n functionality

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            commodi obcaecati tempore, iure quos, hic repudiandae voluptas ad
+            adipisci maiores at eos vero nulla molestiae quidem molestias,
+            soluta eligendi expedita.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+91 8932893212</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 @Tomato.com - All Right reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
