54bce4db2927215f5dfeb5bca8dd89e3b80a21ed
HARSH GARG <officialharshgarg>
2024-08-19 01:29:08 +0530

    Created Login/out functionality

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,6 +5,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartitems] = useState({});
+  const url = "http://localhost:4000";
+  const [token, setToken] = useState("");
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -30,6 +32,12 @@ const StoreContextProvider = (props) => {
   };
 
   useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
+  }, []);
+
+  useEffect(() => {
     console.log(cartItems);
   }, [cartItems]);
 
@@ -40,6 +48,9 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     getTotalCartAmount,
+    url,
+    token,
+    setToken,
   };
 
   return (
